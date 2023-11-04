0382e7921f966c074b98e73e683efd23bc62f4ff
Damian <125214141+TEB-DK@users.noreply.github.com>
2023-11-04 14:51:59 +0100

    TODOapp
    TODOapp

--- App.jsx
+++ App.jsx
@@ -1,64 +1,83 @@
-import { useState } from 'react';
-import './App.css';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, Pressable } from 'react-native'
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("")
+  const [textList, setTextList] = useState([])
+  
+  const [show, setShow] = useState(true)
+  const [reminder, setReminder] = useState(false)
+  
+  const textHandle = (t) => {
+    setText(t)
+  }
 
-  const [wiadomosc, setWiadomosc] = useState("")
-  const [listaWiadomosci, setListaWiadomosci] = useState({
-    lista: []
-  })
+  const buttonHandle = () => {
+    setTextList([...textList, text])
 
-  const wykresl = (id) => {
-    
-    const nowa_lista = (listaWiadomosci.lista).filter(wiadomosc => wiadomosc.id != id)
-    console.log(nowa_lista)
-    
-    setListaWiadomosci(
-      {
-        lista:[
-          ...nowa_lista]
-      }
+    if(show === false){
+      setReminder(true)
+      setTimeout( () => setReminder(false), 250)
+    }
+  }
+
+  const deleteHandle = (idx) => {
+    setTextList(
+      textList.filter((txt, txt_idx) => txt_idx !== idx)
     )
   }
 
-  const obslugaWiadomosci = () => {
-
-    setListaWiadomosci(
-      { ...listaWiadomosci,
-         lista: [ 
-          ...listaWiadomosci.lista,
-          {id: listaWiadomosci.lista.length+1, tekst: wiadomosc, czyWykreslony: false}
-         ]
-      }
-    )
-
-    setWiadomosc("")
+  const showHandle = () => {
+    setShow(!show)
   }
-const obslugaTekstu = (e) => {
-  setWiadomosc(e.target.value)
-}
 
-return (
-  <main>
-    <section>
-      <fieldset className='wiadomosc'>
-        <label htmlFor="dane">Wprowadź wiadomość</label>
-        <input type="text" value={wiadomosc} onChange={obslugaTekstu} name="dane" id="dane" placeholder='Twoja wiadomość' />
-        <button onClick={obslugaWiadomosci}>Wyślij wiadomość</button>
-      </fieldset>
-    </section>
-    <section>
-      <ul>
-        {
-          (listaWiadomosci.lista).map((el) =>
-            (<li key={el.id} className={el.czyWykreslony ? 'wykreslony': 'nie'}>{el.tekst} <input type="checkbox" onChange={() => wykresl(el.id)} /></li>)
+  return (
+    <View>
+
+      <Text>Wprowadź zadanie</Text>
+      <TextInput style={styles.input} value={text} onChangeText={textHandle}></TextInput>
+      <Button title="Dodaj do listy" onPress={buttonHandle}></Button>
+      <Button color={
+        show === true ? ("green") : (
+          reminder ? "red" : "#333"
           )
-        }
-      </ul>
-    </section>
+      }
+        title="Ukryj liste"
+        onPress={showHandle}></Button>
+      <ScrollView style={styles.list}>
+        {show === true ? (
+          textList.map((txt, txt_idx) => (
 
-  </main>
-)
+            <View style={{ flex: 1, justifyContent: "space-evenly", flexDirection: "row" }} key={txt_idx}>
+              <Text style={styles.li}>{txt_idx + 1}.{txt}</Text>
+              <Pressable style={{ backgroundColor: "#444444", width: 35, height: 35 }} onPress={() => deleteHandle(txt_idx)}>
+                <Text style={{ color: "red", textAlign: "center", textAlignVertical: "center", fontSize: 20, padding: 3 }}>X</Text>
+              </Pressable>
+
+            </View>
+
+          )
+          )) : <View></View>
+        }
+      </ScrollView>
+
+    </View>
+  )
 }
 
-export default App;
+export default App
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "gainsboro",
+    padding: 10,
+  },
+  list: {
+    backgroundColor: "#989898",
+  },
+  li: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    color: "white",
+  }
+})
